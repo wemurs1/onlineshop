afd7325d3ad11e8048bc495ac15410ab34d8fdf2
Stuart Murray <stuart.murray.dev@outlook.com>
2026-05-15 12:51:01 +0100

    add ef migration and added banners controller plus views

---
+++ OnlineShop/Areas/Admin/Controllers/BannersController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Models.Database;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BannersController : Controller
+    {
+        private readonly OnlineShopContext _context;
+
+        public BannersController(OnlineShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Banners
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Banners.ToListAsync());
+        }
+
+        // GET: Admin/Banners/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var banner = await _context.Banners
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (banner == null)
+            {
+                return NotFound();
+            }
+
+            return View(banner);
+        }
+
+        // GET: Admin/Banners/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Banners/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,SubTitle,ImageName,Priority,Link,Position")] Banner banner)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(banner);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(banner);
+        }
+
+        // GET: Admin/Banners/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var banner = await _context.Banners.FindAsync(id);
+            if (banner == null)
+            {
+                return NotFound();
+            }
+            return View(banner);
+        }
+
+        // POST: Admin/Banners/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,SubTitle,ImageName,Priority,Link,Position")] Banner banner)
+        {
+            if (id != banner.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(banner);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BannerExists(banner.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(banner);
+        }
+
+        // GET: Admin/Banners/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var banner = await _context.Banners
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (banner == null)
+            {
+                return NotFound();
+            }
+
+            return View(banner);
+        }
+
+        // POST: Admin/Banners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var banner = await _context.Banners.FindAsync(id);
+            if (banner != null)
+            {
+                _context.Banners.Remove(banner);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BannerExists(int id)
+        {
+            return _context.Banners.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ OnlineShop/Migrations/20260515112854_starter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class starter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    SubTitle = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    ImageName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Priority = table.Column<short>(type: "smallint", nullable: true),
+                    Link = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banner", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MenuTitle = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Link = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Banners");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
